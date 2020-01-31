610ac314ecd891b3a9445ddd570d61149c2b41dc
Matthias Bläsing <matthias.blaesing@tu-dortmund.de>
2020-01-31 08:35:14 +0100

    Make profiler available as on OSGI packaged servlet

--- src/main/java/com/papertrail/profiler/jaxrs/CpuProfileResource.java
+++ src/main/java/com/papertrail/profiler/jaxrs/CpuProfileResource.java
@@ -3,48 +3,67 @@ package com.papertrail.profiler.jaxrs;
 import com.papertrail.profiler.CpuProfile;
 import org.joda.time.Duration;
 
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.osgi.service.component.annotations.Component;
 
-@Path("/pprof")
-public class CpuProfileResource {
+@Component(
+        immediate = true,
+        property = {
+            "osgi.http.whiteboard.context.path=/",
+            "osgi.http.whiteboard.servlet.pattern=/profiler/*"
+        },
+        service = Servlet.class
+)
+public class CpuProfileResource extends HttpServlet {
+
     final Lock lock = new ReentrantLock();
 
-    @Produces("pprof/raw")
-    @GET
-    @Path("profile")
-    public byte[] profile(
-            @QueryParam("duration") @DefaultValue("10") Integer duration,
-            @QueryParam("frequency") @DefaultValue("100") Integer frequency) throws IOException {
-        return doProfile(duration, frequency, Thread.State.RUNNABLE);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int duration = 10;
+        int frequency = 100;
+        if (req.getParameter("duration") != null) {
+            duration = Integer.parseInt(req.getParameter("duration"));
+        }
+        if (req.getParameter("frequency") != null) {
+            frequency = Integer.parseInt(req.getParameter("frequency"));
+        }
+        duration = Math.min(duration, 300);
+        frequency = Math.max(1, Math.min(frequency, 1000));
+        if (req.getParameter("mode") == null || "profile".equals(req.getParameter("mode"))) {
+            resp.setHeader("Content-Disposition", "attachment; filename=profile.pprof");
+            resp.setContentType("application/octet-stream");
+            try (OutputStream os = resp.getOutputStream()) {
+                doProfile(duration, frequency, Thread.State.RUNNABLE, os);
+            }
+        } else if ("contention".equals(req.getParameter("mode")))  {
+            resp.setHeader("Content-Disposition", "attachment; filename=profile.pprof");
+            resp.setContentType("application/octet-stream");
+            try (OutputStream os = resp.getOutputStream();) {
+                doProfile(duration, frequency, Thread.State.BLOCKED, os);
+            }
+        } else {
+            resp.sendError(400, "Unknown mode");
+        }
     }
 
-    @Produces("pprof/raw")
-    @GET
-    @Path("contention")
-    public byte[] contention(
-            @QueryParam("duration") @DefaultValue("10") Integer duration,
-            @QueryParam("frequency") @DefaultValue("100") Integer frequency) throws IOException {
-        return doProfile(duration, frequency, Thread.State.BLOCKED);
-    }
-
-    protected byte[] doProfile(int duration, int frequency, Thread.State state) throws IOException {
+    protected void doProfile(int duration, int frequency, Thread.State state, OutputStream os) throws IOException {
         if (lock.tryLock()) {
             try {
                 CpuProfile profile = CpuProfile.record(Duration.standardSeconds(duration), frequency, state);
-                ByteArrayOutputStream stream = new ByteArrayOutputStream();
                 if (profile == null) {
                     throw new RuntimeException("could not create CpuProfile");
                 }
-                profile.writeGoogleProfile(stream);
-                return stream.toByteArray();
+                profile.writeGoogleProfile(os);
+                return;
             } finally {
                 lock.unlock();
             }
